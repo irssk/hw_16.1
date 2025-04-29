c74ccac0097bbab82488afebed8b5efebeb395ea
Ira <ira350520@gmail.com>
2025-04-29 12:37:44 +0300

    completed homework

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the path to the source file: ");
+        string sourcePath = Console.ReadLine();
+
+        Console.Write("Enter the path to the file you want to copy the data to: ");
+        string destinationPath = Console.ReadLine();
+
+        try
+        {
+            if (!File.Exists(sourcePath))
+            {
+                Console.WriteLine("The source file does not exist.");
+                return;
+            }
+
+            File.Copy(sourcePath, destinationPath, true);
+
+            Console.WriteLine("File copied successfully!");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred while copying the file:");
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
